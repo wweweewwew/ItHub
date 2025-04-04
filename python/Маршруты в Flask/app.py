b6df65d8bedeb848e9b7a655d2e3109f32bcca4e
wweweewwew <tprpra6@gmail.com>
2025-04-04 16:50:30 +0300

    ready py kt 2

---
+++ python/Маршруты в Flask/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import datetime  # Импортируем модуль полностью
+
+app = Flask(__name__)
+
+def get_greeting():
+    now = datetime.datetime.now()
+    
+    if 6 <= now.hour < 12:
+        return "Доброе утро"
+    elif 12 <= now.hour < 18:
+        return "Добрый день"
+    elif 18 <= now.hour < 24:
+        return "Добрый вечер"
+    else:
+        return "Доброй ночи"
+
+@app.route('/')
+def index():
+    return render_template('index.html', greeting=get_greeting())
+
+if __name__ == '__main__':
+    app.run(debug=True)
